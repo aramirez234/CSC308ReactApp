a133fdec01cabccedca7830f4ae1045fef04ae97
aramirez234 <arami202@calpoly.edu>
2022-01-05 15:45:28 -0800

    Changed Table

--- src/Table.js
+++ src/Table.js
@@ -3,32 +3,46 @@ import React from 'react';
 function Table() {
     return (
         <table>
-            <thead>
+            <TableHeader />
+            <TableBody /> 
+        </table>
+    );
+}
+
+
+function TableHeader()
+{
+    return (
+        <thead>
                 <tr>
                     <th>Name</th>
                     <th>Job</th>
                 </tr>
             </thead>
-            <body>
-                <tr>
-                    <td>Charlie</td>
-                    <td>Janitor</td>
-                </tr>
-                <tr>
-                    <td>Mac</td>
-                    <td>Bouncer</td>
-                </tr>
-                <tr>
-                    <td>Dee</td>
-                    <td>Aspiring actress</td>
-                </tr>
-                <tr>
-                    <td>Dennis</td>
-                    <td>Bartender</td>
-                </tr>
-            </body>
-        </table>
     );
+}
+
+function TableBody()
+{
+    return(<body>
+        <tr>
+            <td>Charlie</td>
+            <td>Janitor</td>
+        </tr>
+        <tr>
+            <td>Mac</td>
+            <td>Bouncer</td>
+        </tr>
+        <tr>
+            <td>Dee</td>
+            <td>Aspiring actress</td>
+        </tr>
+        <tr>
+            <td>Dennis</td>
+            <td>Bartender</td>
+        </tr>
+    </body>);
+
 }
 
 export default Table;
