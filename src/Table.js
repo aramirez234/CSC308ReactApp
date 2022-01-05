37d5d0656cc046895015aa29ce5d4fdad420a28a
aramirez234 <arami202@calpoly.edu>
2022-01-05 15:38:43 -0800

    Second Commit

---
+++ src/Table.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function Table() {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Job</th>
+                </tr>
+            </thead>
+            <body>
+                <tr>
+                    <td>Charlie</td>
+                    <td>Janitor</td>
+                </tr>
+                <tr>
+                    <td>Mac</td>
+                    <td>Bouncer</td>
+                </tr>
+                <tr>
+                    <td>Dee</td>
+                    <td>Aspiring actress</td>
+                </tr>
+                <tr>
+                    <td>Dennis</td>
+                    <td>Bartender</td>
+                </tr>
+            </body>
+        </table>
+    );
+}
+
+export default Table;
