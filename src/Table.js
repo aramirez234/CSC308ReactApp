b954dd7d1bd9268b9a4a9a21d0d9379da87dff38
aramirez234 <arami202@calpoly.edu>
2022-01-05 19:10:57 -0800

    Finished React Lab

--- src/Table.js
+++ src/Table.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function Table() {
+function Table(props) {
     return (
         <table>
             <TableHeader />
-            <TableBody /> 
+            <TableBody characterData = {props.characterData}/>
         </table>
     );
 }
@@ -18,30 +18,27 @@ function TableHeader()
                     <th>Name</th>
                     <th>Job</th>
                 </tr>
-            </thead>
+        </thead>
     );
 }
 
-function TableBody()
+function TableBody(props)
 {
-    return(<body>
-        <tr>
-            <td>Charlie</td>
-            <td>Janitor</td>
-        </tr>
-        <tr>
-            <td>Mac</td>
-            <td>Bouncer</td>
-        </tr>
-        <tr>
-            <td>Dee</td>
-            <td>Aspiring actress</td>
-        </tr>
-        <tr>
-            <td>Dennis</td>
-            <td>Bartender</td>
-        </tr>
-    </body>);
+    const rows = props.characterData.map( (row,index) => {
+        return(
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+            </tr>
+        );
+    });
+
+
+    return(
+    <tbody>
+        {rows}
+    </tbody>
+    );
 
 }
 
